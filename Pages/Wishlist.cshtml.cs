cb2af3ead5bc2e145b26846b9b47792b117ffde0
chathunii <chathu38bbc@gmail.com>
2025-01-04 20:57:00 +0530

    wishlist page done

---
+++ Pages/Wishlist.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shift_To_Thrift_Platform.Pages
+{
+    public class WishlistModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
